67a7eeef97bfc1f57835209b5e447ac5587ecd50
sijben (Stef Sijben) <Stef.Sijben@ruhr-uni-bochum.de>
2015-04-13 15:51:08 +0000

    Some fixes in classification algorithm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bbmm-analysis@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/classification.R
+++ pkg/R/classification.R
@@ -28,18 +28,21 @@ setMethod(f = "classify",
 	
 	## First, ensure the LL functions are sorted by increasing arg max
 	M <- apply(LL, 2, which.max)
-	LL <- LL[,order(M)]
+	LL[,seq_len(ncol(LL))] <- LL[,order(M)]
 	M <- M[order(M)] ## In which iteration a trajectory gets included
 	fn.until <- findInterval(0:(nrow(LL)-1), M) ## Opt_i classifies functions 1 to fn.until[i]
 	
-	## An arbitrary classification to initialize with
-	C <- rep(1, ncol(LL))
-	attr(C, "IC") <- sum(LL[1,]) - p
+	## The classification with everything at x_1 to initialize with
+	C <- rep(NA, ncol(LL))
+	attr(C, "IC") <- -Inf
 		
 	Opt <- matrix(NA, nrow=nrow(LL)+1, ncol=ncol(LL))
-	for (i in 2:nrow(LL)) {
-		ICi.Opt <- -Inf
-		for (j in 1:(i-1)) {
+	for (i in 1:nrow(LL)) {
+		## Everything at x_i as extension of Opt_0
+		Opt[i,seq_len(fn.until[i])] <- i
+		ICi.Opt <- sum(LL[i,seq_len(fn.until[i])]) - p
+		
+		for (j in seq_len(i-1)) {
 			## Extend Opt_j to a candidate for Opt_i
 			O <- Opt[j,]
 			if (fn.until[j] < fn.until[i]) {
@@ -62,13 +65,18 @@ setMethod(f = "classify",
 		if (fn.until[i] < ncol(LL)) {
 			Ci[(fn.until[i]+1):ncol(LL)] <- i
 		}
+		
+		## If Ci is better than C, replace it
 		LL.Ci <- apply(cbind(1:ncol(LL), Ci), 1, function(idx) { LL[idx[2],idx[1]] })
 		IC.Ci <- sum(LL.Ci) - p*length(unique(Ci))
-		
 		if (IC.Ci > attr(C, "IC")) {
 			C <- Ci
 			attr(C, "IC") <- IC.Ci
 		}
+	}
+	print(attributes(LL))
+	if (!is.null(attr(LL, "dc.values"))) {
+		attr(C, "dc.values") <- attr(LL, "dc.values")[C]
 	}
 	C
 }
